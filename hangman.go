02100d4c8e8924ed24aaaf18f3144261299b209e
NavaneethaPrasad <navaneethaprasad25092002@gmail.com>
2025-10-15 13:37:18 +0530

    print all words line by line

--- hangman.go
+++ hangman.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func getSecretWord(wordFileName string) string {
+	file, err := os.Open(wordFileName)
+	if err != nil {
+		fmt.Errorf("The file could not open", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 	return "navaneetha"
-
 }
 
 func main() {
