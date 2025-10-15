665d28e8da963e92ab52f2d79e2b1d42ed2ee24d
NavaneethaPrasad <navaneethaprasad25092002@gmail.com>
2025-10-15 17:02:57 +0530

    GREEN:Add function for no punctuation

--- hangman.go
+++ hangman.go
@@ -6,19 +6,29 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"unicode"
 )
 
+func isAllLetters(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
 func getSecretWord(wordFileName string) string {
 	allowedWords := []string{}
 	file, err := os.Open(wordFileName)
 	if err != nil {
-		fmt.Errorf("The file could not open:%v", err)
+		errMessage := fmt.Sprintf("Can't open file %s : %v\n", wordFileName, err)
+		panic(errMessage)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		word := scanner.Text()
-		if word == strings.ToLower(word) && len(word) >= 6 {
+		if word == strings.ToLower(word) && len(word) >= 6 && isAllLetters(word) {
 			allowedWords = append(allowedWords, word)
 		}
 	}
