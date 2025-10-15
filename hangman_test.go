6b0ad4682376293338f5d3015685fb329f8fcbb5
NavaneethaPrasad <navaneethaprasad25092002@gmail.com>
2025-10-15 15:35:43 +0530

    RED:Add testcase for check secret word has at least 6 letters

--- hangman_test.go
+++ hangman_test.go
@@ -13,3 +13,11 @@ func TestSecretWordNoCapitals(t *testing.T) {
 	}
 
 }
+
+func TestSecretWordLength(t *testing.T) {
+	wordList := "/usr/share/dict/words"
+	secretWord := getSecretWord(wordList)
+	if len(secretWord) < 6 {
+		t.Errorf("Expected word length 6 or greater than 6, but got %q (length %d)", secretWord, len(secretWord))
+	}
+}
