f6a919d410300c26d1947438f4f819eb5ee432e0
NavaneethaPrasad <navaneethaprasad25092002@gmail.com>
2025-10-17 11:21:25 +0530

    RED:Add test case for correct guess when full chances remain

--- hangman_test.go
+++ hangman_test.go
@@ -52,3 +52,34 @@ func TestSecretWordNopunctuation(t *testing.T) {
 		t.Errorf("Should not get words with punctuations. Got %s", secretWord)
 	}
 }
+
+func TestCorrectGuess(t *testing.T) {
+	// currentState := Hangman{
+	// 	secretWord:     "elephant",
+	// 	guesses:        []byte{},
+	// 	chancesLeft:    7,
+	// 	correctGuesses: []byte{},
+	// }
+	secretWord := "elephant"
+	currentState := NewHangman(secretWord)
+	user_Input := byte('t')
+	newState := checkGuess(currentState, byte(user_Input))
+	expected := Hangman{
+		secretWord:     secretWord,
+		guesses:        append(currentState.guesses, byte(user_Input)),
+		chancesLeft:    7,
+		correctGuesses: append(currentState.correctGuesses, byte(user_Input)),
+	}
+	if newState.secretWord != expected.secretWord {
+		t.Errorf("Secreat word is modified\n")
+	}
+	if string(newState.guesses) != string(expected.guesses) {
+		t.Errorf("Guess should be [t] but got %v\n", newState.guesses)
+	}
+	if string(newState.correctGuesses) != string(expected.correctGuesses) {
+		t.Errorf("Guess should be [t] but got %v", newState.correctGuesses)
+	}
+	if newState.chancesLeft != expected.chancesLeft {
+		t.Errorf("Chances left modified!\n")
+	}
+}
